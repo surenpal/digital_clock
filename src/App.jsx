f35dd612eee9adea692941340ba84a30e0926ed5
suren <surenpal666@gmail.com>
2026-01-11 14:56:27 +0900

    useState and useEffect code is added to the component file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 function App() {
   return (
   <div className="bg-black text-red-100 h-screen w-full "><h1>Digital clock on the way guys</h1>
+  <DigitalClock />
   </div>
   )
 }
--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export const DigitalClock = () => {
+const [time, setTime] = useState(new Date());
+
+useEffect(() => {
+  const timerID = setInterval(() => {
+    setTime(new Date());
+  }, 1000);
+  return () => clearInterval(timerID);
+}, []);
+
   return (
     <div>
         <h1>Digital Clock Component</h1>
